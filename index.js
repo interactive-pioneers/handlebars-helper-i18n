128910a5b2ba344d2e78a03c5d460f43f46c15e3
Ain Tohvri <at@interactive-pioneers.de>
2016-07-19 14:00:50 +0200

    Add nested key support based on Handlebars 1 API

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
 /**
- * {{i18n}} <http://github.com/helpers/handlebars-helper-i18n>
- *
- * Copyright (c) 2014 Laurent Goderre <https://github.com/LaurentGoderre>
- * Licensed under the MIT License (MIT)
+ * Handlebars Helpers: i18n, for localizing templates in multiple languages.
+ * Copyright (c) 2013 Jon Schlinkert, Brian Woodward, contributors
+ * Licensed under the MIT License (MIT).
  */
-
 'use strict';
 
 
-/**
- * @param  {String} `key` The name of the property to use as context.
- * @param  {Object} `options`
- * @return {Object}
- */
+// The module to be exported
+var helpers = {
 
-exports.i18n = function (key, options) {
+  /**
+   * {{i18n}}
+   * @author: Laurent Goderre <https://github.com/LaurentGoderrre>
+   * @param  {String} context
+   * @param  {Object} options
+   * @return {String}
+   * @example: <https://github.com/assemble/buttons> (See the "button-i18n" example)
+   */
+  i18n: function (context, options) {
+    var language = void 0;
+    if (typeof context !== "string") {
+      throw "Key must be of type 'string'";
+    }
+    var scope = options.hash.scope ? options.hash.scope : this;
+    language = (typeof options.hash.language === "string" ? options.hash.language : scope.language);
+    if (typeof language === "undefined") {
+      throw "The 'language' parameter is not defined";
+    }
+    if (typeof scope[language] === "undefined") {
+      throw "No strings found for language '" + language + "'";
+    }
+
+    // Find translation by notation key, e.g. meta.keywords
+    // Return null if not found to support conditions, e.g.
+    //  {{#if (i18n "has.this.key") }}
+    var findTranslation = function(translation, parts) {
+      var length = parts.length;
+      var firstElement = parts.shift();
+      if (length === 1) {
+        if (translation[firstElement]) {
+          return translation[firstElement];
+        }
+        else {
+          return null;
+        }
+      }
+      else {
+        if (translation[firstElement]) {
+          return findTranslation(translation[firstElement], parts);
+        }
+        else {
+          return null;
+        }
+      }
+    };
+    var idParts = context.split('.');
+    return findTranslation(scope[language], idParts);
+  }
+};
+
+
+// Export helpers
+module.exports.register = function (Handlebars, options) {
   options = options || {};
-  options.hash = options.hash || {};
-
-  var language;
-
-  if (typeof key !== "string") {
-    throw "{{i18n}} helper: invalid key. Object keys must be formatted as strings.";
+  for (var helper in helpers) {
+    if (helpers.hasOwnProperty(helper)) {
+      Handlebars.registerHelper(helper, helpers[helper]);
+    }
   }
-
-  if (typeof options.hash.language === "string") {
-    language = options.hash.language;
-  } else {
-    language = this.language;
-  }
-
-  if (typeof language === "undefined") {
-    throw "{{i18n}} helper: the 'language' parameter is not defined.";
-  }
-
-  if (typeof this[language] === "undefined") {
-    throw "{{i18n}} helper: context object not defined for language '" + language + "'.";
-  }
-
-  if (typeof this[language][key] === "undefined") {
-    throw "{{i18n}} helper: property '" + key + "' is not defined for language '" + language + "'.";
-  }
-
-  return this[language][key];
 };
